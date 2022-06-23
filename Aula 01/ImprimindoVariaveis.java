45ebd65c1315a2a869ac3ad44c30a60b9e69f6be
Yuri Mello <58647179+YuriMelloFiles@users.noreply.github.com>
2022-06-23 17:47:48 -0300

    Add files via upload

---
+++ Aula 01/ImprimindoVariaveis.java
@@ -0,0 +1,9 @@
+
+public class ImprimindoVariaveis {
+	public static void main(String[] args){
+		System.out.println("Dados impressos again");
+		// criando a variável idadeDaFilha
+		int idadeDaFilha;
+	}
+}
+
