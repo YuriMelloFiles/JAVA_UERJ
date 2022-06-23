681d300d3543875ab554e0aef52c147cfb466171
Yuri Mello <58647179+YuriMelloFiles@users.noreply.github.com>
2022-06-23 20:38:04 +0000

    Delete Aula 1 directory

--- Aula 1/ImprimindoVariaveis.java
+++
@@ -1,9 +0,0 @@
-
-public class ImprimindoVariaveis {
-	public static void main(String[] args){
-		System.out.println("Dados impressos again");
-		// criando a variável idadeDaFilha
-		int idadeDaFilha;
-	}
-}
-
